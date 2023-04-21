fbffce348b235ccfe1c0386726faad4048e93a99
Rudra Saraswat <rs2009@ubuntu.com>
2023-04-21 14:58:36 +0530

    Update list of filesystems and mountpoints available

--- jade_gui/manualpartitioning.py
+++ jade_gui/manualpartitioning.py
@@ -1,16 +1,10 @@
 filesystems = [
     "don't format",
-    "bfs",
-    "cramfs",
-    "ext3",
     "fat",
-    "msdos",
     "xfs",
     "btrfs",
     "ext2",
     "ext4",
-    "minix",
-    "vfat",
     "f2fs",
 ]
 
@@ -20,8 +14,4 @@ mountpoints = [
     "/boot",
     "/boot/efi",
     "/home",
-    "/opt",
-    "/tmp",
-    "/usr",
-    "/var",
 ]
