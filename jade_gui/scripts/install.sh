9dc81b3c189265528e15f78052a59f1429ba208f
Rudra Saraswat <rs2009@ubuntu.com>
2023-05-14 19:32:36 +0530

    chore: fix typo in install.sh

--- jade_gui/scripts/install.sh
+++ jade_gui/scripts/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 echo | tee /tmp/jade-gui-output.txt &>/dev/null
 echo "Starting installation with blend-inst..." | tee -a /tmp/jade-gui-output.txt
-pkexec blend-inst config ~/.config/jade.json 2>&/dev/null | tee -a /tmp/jade-gui-output.txt
+pkexec blend-inst config ~/.config/jade.json 2>/dev/null | tee -a /tmp/jade-gui-output.txt
